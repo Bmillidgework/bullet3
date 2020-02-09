cdc36b774331b1b1e05f6ab6abc5ea22c669eb8f
Beren <bmillidgework@gmail.com>
2020-02-09 19:41:01 +0000

    trying to stop losing connection to bullet engine

--- examples/pybullet/gym/pybullet_utils/bullet_client.py
+++ examples/pybullet/gym/pybullet_utils/bullet_client.py
@@ -29,14 +29,14 @@ class BulletClient(object):
         connection_mode = pybullet.DIRECT
     self._client = pybullet.connect(connection_mode)
 
-  def __del__(self):
-    """Clean up connection if not already done."""
-    if self._client>=0:
-      try:
-        pybullet.disconnect(physicsClientId=self._client)
-        self._client = -1
-      except pybullet.error:
-        pass
+ #def __del__(self):
+ #   """Clean up connection if not already done."""
+ #   if self._client>=0:
+ #     try:
+ #       pybullet.disconnect(physicsClientId=self._client)
+ #       self._client = -1
+ #     except pybullet.error:
+ #       pass
 
   def __getattr__(self, name):
     """Inject the client id into Bullet functions."""
